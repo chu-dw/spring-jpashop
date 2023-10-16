6c7014999ebe638b0e5fc1aca012d1da48bad3e3
DongWon <ehddnjs1747@daum.net>
2023-10-16 15:46:13 +0900

    ItemService

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -1,2 +1,32 @@
-package jpabook.jpashop.repository;public class ItemRepository {
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){  //id 값이 없다는건 새로운 객체(신구 등록)
+            em.persist((item));
+        } else{ //이미 db에 등록 (업데이트 느낌)
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
 }
