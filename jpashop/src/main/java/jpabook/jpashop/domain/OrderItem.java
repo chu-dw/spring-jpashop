c0814b91d7f053a56aff45e15bb4727fd4f70d8a
DongWon <ehddnjs1747@daum.net>
2023-10-17 14:27:13 +0900

    Order repository

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,6 +25,16 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    //==생성메서드==/
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //주문한 만큼 item 쩨고 내려줌
+        return orderItem;
+    }
 
     //==비지니스 로직==//
     public void cancel(){   //취소된 제고 수량을 원삭복구
